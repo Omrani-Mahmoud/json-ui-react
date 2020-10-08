862311c5361b7013bb4ea8a36b109235429e3b30
facecheckdisnuts <omranimahmoud@gmail.com>
2020-10-08 18:10:34 +0100

    Update example

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import { ExampleComponent } from 'f'
+import { Json_display } from 'json-ui-react'
 import 'f/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <Json_display />
 }
 
 export default App
